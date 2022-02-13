0918892bcce2daeeb8ba172b7d7aa809012eb1cb
0xChrisJones <0xchrisjones@gmail.com>
2022-02-13 19:59:15 +0100

    Exported Apollo client

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 import Link from 'next/link';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-const client = new ApolloClient({
+export const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
 });
