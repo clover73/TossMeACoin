67b27080bda638e081de9e8656129ec7562b2d9a
0xChrisJones <0xchrisjones@gmail.com>
2022-02-13 20:02:44 +0100

    Added ISR

--- client/pages/[publicKey].js
+++ client/pages/[publicKey].js
@@ -1,10 +1,62 @@
-import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { gql } from '@apollo/client';
+import { client } from './_app';
 
-const SupportPage = () => {
-  const router = useRouter();
-  const { publicKey } = router.query;
+const SupportPage = ({ creator }) => {
+  const [receiverBalance, setReceiverBalance] = useState(0);
+  const [donationsCount, setDonationsCount] = useState(0);
 
-  return <p>Public Key: {publicKey}</p>;
+  return <p>Creator: {creator.name}</p>;
 };
 
 export default SupportPage;
+
+export async function getStaticProps({ params }) {
+  const { publicKey } = params;
+
+  const { data } = await client.query({
+    query: gql`
+    {
+      creator(publicKey: "${publicKey}") {
+        publicKey
+        name
+        bio
+        createdAt
+        avatarURL
+        bannerURL
+        Twitter
+        Instagram
+        YouTube
+        TikTok
+        LinkedIn
+        GitHub
+        Website
+      }
+    }`,
+  });
+
+  return {
+    props: {
+      creator: data.creator,
+    },
+    revalidate: 10,
+  };
+}
+
+export async function getStaticPaths() {
+  const { data } = await client.query({
+    query: gql`
+      {
+        creators {
+          publicKey
+        }
+      }
+    `,
+  });
+
+  const paths = data.creators.map((creator) => ({
+    params: { publicKey: creator.publicKey },
+  }));
+
+  return { paths, fallback: 'blocking' };
+}
