06e68e59b1c98b837b05dbf170370ada221222b2
0xChrisJones <0xchrisjones@gmail.com>
2022-02-14 18:18:23 +0100

    Fixed an error

--- smartcontracts/contracts/TossMeACoin.sol
+++ smartcontracts/contracts/TossMeACoin.sol
@@ -1,8 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "../node_modules/hardhat/console.sol";
-
 contract TossMeACoin {
   uint256 public totalDonationsCount;
   address owner;
