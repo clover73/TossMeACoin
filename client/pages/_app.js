45264befcb632cacdedcc7e493e8b218bbc9b946
0xChrisJones <0xchrisjones@gmail.com>
2022-02-14 20:34:25 +0100

    Wrapped the app in Provider

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import Link from 'next/link';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { TMACProvider } from '../context/TossMeACoinContext';
 
 export const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
@@ -10,16 +11,18 @@ export const client = new ApolloClient({
 const MyApp = ({ Component, pageProps }) => {
   return (
     <ApolloProvider client={client}>
-      <nav>
-        <div>Logo</div>
-        <div>
-          <Link href="/creators">
-            <a>Explore creators</a>
-          </Link>
-          <a>Connect wallet</a>
-        </div>
-      </nav>
-      <Component {...pageProps} />{' '}
+      <TMACProvider>
+        <nav>
+          <div>Logo</div>
+          <div>
+            <Link href="/creators">
+              <a>Explore creators</a>
+            </Link>
+            <a>Connect wallet</a>
+          </div>
+        </nav>
+        <Component {...pageProps} />{' '}
+      </TMACProvider>
     </ApolloProvider>
   );
 };
