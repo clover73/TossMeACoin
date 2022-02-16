3fa1cf6ecc3514c231eb1a5b2293b7d28e72029f
0xChrisJones <0xchrisjones@gmail.com>
2022-02-16 20:23:46 +0100

    Added simple auth

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,9 +3,17 @@ import Link from 'next/link';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { TMACProvider } from '../context/TossMeACoinContext';
 
+const token = Buffer.from(
+  `${process.env.API_USER}:${process.env.PASSWORD}`,
+  'utf8'
+).toString('base64');
+
 export const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
+  headers: {
+    Authorization: `Basic ${token}`,
+  },
 });
 
 const MyApp = ({ Component, pageProps }) => {
