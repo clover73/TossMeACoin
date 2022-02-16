90746c0c8768d19aa1b74d77d4297a574fbfa5a2
0xChrisJones <0xchrisjones@gmail.com>
2022-02-16 18:46:08 +0100

    Fixed CORS

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: '.env' });
 
 const express = require('express');
+const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
 const schema = require('./schema/schema');
@@ -12,6 +13,7 @@ mongoose.connection.once('open', () => {
   console.log('Connected to database.');
 });
 
+app.use(cors());
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(4000, () => {
