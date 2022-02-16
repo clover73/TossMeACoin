21499a1b0ad835c93c8744a0b9db956bd829da19
0xChrisJones <0xchrisjones@gmail.com>
2022-02-16 20:10:52 +0100

    Added a basic auth

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ require('dotenv').config({ path: '.env' });
 
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+const basicAuth = require('express-basic-auth');
 const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
 const schema = require('./schema/schema');
@@ -13,7 +15,13 @@ mongoose.connection.once('open', () => {
   console.log('Connected to database.');
 });
 
+app.use(helmet());
 app.use(cors());
+app.use(
+  basicAuth({
+    users: { admin: process.env.PASSWORD },
+  })
+);
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(4000, () => {
