46b539a68d1e86467a958c18416108328d1e2e45
0xChrisJones <0xchrisjones@gmail.com>
2022-02-19 18:50:29 +0100

    Added small logo

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -9,17 +9,23 @@ const Navbar = () => {
   return (
     <nav>
       <div className="max-w-6xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center sm:justify-between justify-center h-16">
-          <div>
-            <Link href="/">
+        <div className="relative flex items-center justify-between h-16">
+          <Link href="/">
+            <div className="flex-shrink-0 flex items-center">
               <img
-                className="h-16 w-auto cursor-pointer"
+                className="hidden sm:block h-16 w-auto cursor-pointer"
                 src="/logo.svg"
                 alt="Toss Me A Coins"
               />
-            </Link>
-          </div>
-          <div className="hidden sm:block">
+              <img
+                className="block sm:hidden h-16 w-auto cursor-pointer"
+                src="/logoSmall.svg"
+                alt="Toss Me A Coins"
+              />
+            </div>
+          </Link>
+
+          <div>
             {account ? (
               <div className="bg-[#5e17eb] text-white font-bold py-2 px-4 rounded-full">{`${account.substring(
                 0,
