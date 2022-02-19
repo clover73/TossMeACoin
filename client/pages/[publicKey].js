45d80568ab1344184c46022cfc1d9a8327eb5ee9
0xChrisJones <0xchrisjones@gmail.com>
2022-02-19 21:42:39 +0100

    Added isLoggedIn condition

--- client/pages/[publicKey].js
+++ client/pages/[publicKey].js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { useState, useContext, useEffect } from 'react';
 import { gql } from '@apollo/client';
 import { client } from './_app';
@@ -14,10 +15,11 @@ const SupportPage = ({ publicKey, creator }) => {
     sentDonations,
   } = useContext(TMACContext);
 
-  const [dbData, setDBData] = useState(null);
   const [name, setName] = useState('');
   const [msg, setMsg] = useState('');
   const [amount, setAmount] = useState('');
+
+  const isLoggedIn = account == publicKey.toLocaleLowerCase();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -39,108 +41,86 @@ const SupportPage = ({ publicKey, creator }) => {
     }
   }, [isLoading]);
 
-  useEffect(async () => {
-    try {
-      const { data } = await client.query({
-        query: gql`
-        {
-          creator(publicKey: "${publicKey}") {
-            publicKey
-            name
-            bio
-            createdAt
-            avatarURL
-            bannerURL
-            Twitter
-            Instagram
-            YouTube
-            TikTok
-            LinkedIn
-            GitHub
-            Website
-          }
-        }`,
-      });
-      setDBData(data.creator);
-    } catch (error) {
-      console.error(error.message);
-    }
-  }, []);
-
   return (
-    <div>
-      <h2>Send donations to {publicKey}</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:{' '}
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <label>
-          Message:{' '}
-          <input
-            type="text"
-            value={msg}
-            onChange={(e) => setMsg(e.target.value)}
-          />
-        </label>
-        <label>
-          Amount:{' '}
-          <input
-            type="number"
-            step="0.0001"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-          />
-        </label>
-        {account ? (
-          isLoading ? (
-            <p>Loading...</p>
+    <>
+      <Head>
+        <title>{publicKey} Toss Me A Coin</title>
+      </Head>
+
+      <main className="flex items-center justify-center min-h-[80vh]">
+        <h2>Send donations to {publicKey}</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:{' '}
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Message:{' '}
+            <input
+              type="text"
+              value={msg}
+              onChange={(e) => setMsg(e.target.value)}
+            />
+          </label>
+          <label>
+            Amount:{' '}
+            <input
+              type="number"
+              step="0.0001"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+            />
+          </label>
+          {account ? (
+            isLoading ? (
+              <p>Loading...</p>
+            ) : (
+              <input type="submit" value="Send Donation" />
+            )
           ) : (
-            <input type="submit" value="Send Donation" />
-          )
-        ) : (
-          <button onClick={connectWallet}>Connect wallet</button>
+            <button onClick={connectWallet}>Connect wallet</button>
+          )}
+        </form>
+        {isLoggedIn && (
+          <div>
+            <div>
+              <h2>Your Donations</h2>
+              {sentDonations && sentDonations.length != 0 ? (
+                sentDonations.map((donation) => (
+                  <div key={donation.timestamp}>
+                    <p>Sent to {donation.addressTo}</p>
+                    <p>Amount {donation.amount}</p>
+                    <p>Name {donation.name}</p>
+                    <p>Message {donation.message}</p>
+                  </div>
+                ))
+              ) : (
+                <p>No donations sent</p>
+              )}
+            </div>
+            <div>
+              <h2>Donations Received</h2>
+              {receivedDonations && receivedDonations.length != 0 ? (
+                receivedDonations.map((donation) => (
+                  <div key={donation.timestamp}>
+                    <p>Received from {donation.addressFrom}</p>
+                    <p>Amount {donation.amount}</p>
+                    <p>Name {donation.name}</p>
+                    <p>Message {donation.message}</p>
+                  </div>
+                ))
+              ) : (
+                <p>No donations received</p>
+              )}
+            </div>
+          </div>
         )}
-      </form>
-      {account && (
-        <div>
-          <div>
-            <h2>Your Donations</h2>
-            {sentDonations ? (
-              sentDonations.map((donation) => (
-                <div key={donation.timestamp}>
-                  <p>Sent to {donation.addressTo}</p>
-                  <p>Amount {donation.amount}</p>
-                  <p>Name {donation.name}</p>
-                  <p>Message {donation.message}</p>
-                </div>
-              ))
-            ) : (
-              <p>No donations sent</p>
-            )}
-          </div>
-          <div>
-            <h2>Donations Received</h2>
-            {receivedDonations ? (
-              receivedDonations.map((donation) => (
-                <div key={donation.timestamp}>
-                  <p>Received from {donation.addressFrom}</p>
-                  <p>Amount {donation.amount}</p>
-                  <p>Name {donation.name}</p>
-                  <p>Message {donation.message}</p>
-                </div>
-              ))
-            ) : (
-              <p>No donations recived</p>
-            )}
-          </div>
-        </div>
-      )}
-    </div>
+      </main>
+    </>
   );
 };
 
@@ -154,7 +134,7 @@ export async function getStaticProps({ params }) {
   const { data } = await client.query({
     query: gql`
     {
-      creator(publicKey: "${publicKey}") {
+      creator(publicKey: "${publicKey.toLocaleLowerCase()}") {
         name
         bio
         createdAt
