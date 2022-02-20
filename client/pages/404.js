0bac71d22bbdb08b7259e842b1a293ce16d3c533
0xChrisJones <0xchrisjones@gmail.com>
2022-02-20 16:43:04 +0100

    Added custom 404

---
+++ client/pages/404.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+const custom404 = () => {
+  return (
+    <>
+      <Head>
+        <title>404 - This page doesn't exist</title>
+      </Head>
+      <main className="flex items-center justify-center min-h-[80vh] m-4">
+        <h1 className="text-5xl md:text-7xl text-center font-extrabold leading-tighter tracking-tighter mb-4">
+          404
+        </h1>
+      </main>
+    </>
+  );
+};
+
+export default custom404;
